ecb01bf477b56d23b13fc92d0bb3337a91dfe6eb
Jordan Jozwiak <jordanjoz1@gmail.com>
2015-01-02 21:21:48 -0800

    initial commit

---
+++ test/test_localization_helper.py
@@ -0,0 +1,143 @@
+import unittest
+import os
+import xml.etree.ElementTree as ET
+import translation_helper as localizr
+import filecmp
+
+class TestLocalizationHelperFunctions(unittest.TestCase):
+
+    
+    def setUp(self):
+        # constants
+        self.LANGS = ['de', 'es', 'fr','zh-rTW']
+
+        # save cwd so we can revert back to it when we're done with a test
+        self.cwd = os.getcwd()
+
+        # get absolute path to resource directory
+        self.res_path = os.path.abspath('./res')
+
+        # get default strings tree
+        self.tree = localizr.getDefaultTree(self.res_path)
+
+    def tearDown(self):
+        os.chdir(self.cwd)
+
+    def test_writeOutMissingStrings(self):
+        # get tags from default tree so we know what to write out
+        tags = localizr.getTagsFromTree(self.tree)
+
+        # get the missing keys for each lang
+        keys = localizr.getKeysFromTree(self.tree)
+        missing = localizr.findMissingKeys(keys, self.LANGS, self.res_path)
+
+        # write out the files
+        localizr.writeMissingKeysToFiles(self.LANGS, tags, missing)
+
+        # verify that no German strings exist
+        os.chdir(self.cwd)
+        self.assertFalse(os.path.exists('./to_translate/strings_to_trans-de.xml'))
+
+        # verify that spanish strings seem correct
+        os.chdir(self.cwd)
+        self.assertTrue(os.path.exists('./to_translate/strings_to_trans-es.xml'))
+        self.assertTrue(filecmp.cmp('./test_to_translate/strings_to_trans-es.xml', './to_translate/strings_to_trans-es.xml'))
+
+        # verify that french strings seem correct
+        os.chdir(self.cwd)
+        self.assertTrue(os.path.exists('./to_translate/strings_to_trans-fr.xml'))
+        self.assertTrue(filecmp.cmp('./test_to_translate/strings_to_trans-fr.xml', './to_translate/strings_to_trans-fr.xml'))
+
+        # verify that traditional chinese strings seem correct
+        os.chdir(self.cwd)
+        self.assertTrue(os.path.exists('./to_translate/strings_to_trans-zh-rTW.xml'))
+        self.assertTrue(filecmp.cmp('./test_to_translate/strings_to_trans-zh-rTW.xml', './to_translate/strings_to_trans-zh-rTW.xml'))
+
+    def test_getLanguageTrees(self):
+        trees = localizr.getLanguageTrees(self.LANGS, self.res_path)
+        self.assertEquals(len(trees), 4)
+
+    '''
+    ' Extra strings will be removed and re-arranged to match the order of the strings
+    ' in the default language
+    '''
+    def test_cleanTranslationFiles(self):
+        # get the keys from the default language
+        keys = localizr.getKeysFromTree(self.tree)
+
+        # clean the fiels
+        localizr.createOutputDir()
+        localizr.cleanTranslationFiles(self.LANGS, keys, self.res_path)
+
+        # verify that German strings are correctly sorted
+        os.chdir(self.cwd)
+        self.assertTrue(filecmp.cmp('./res/values-de/strings.xml', './test_cleaned/res/values-de/strings.xml'))
+
+        # verify that empty spanish strings stays empty
+        os.chdir(self.cwd)
+        self.assertTrue(filecmp.cmp('./res/values-es/strings.xml', './test_cleaned/res/values-es/strings.xml'))
+
+        # verify that extra french string is removed
+        os.chdir(self.cwd)
+        self.assertTrue(filecmp.cmp('./res/values-fr/strings.xml', './test_cleaned/res/values-fr/strings.xml'))
+
+        # verify that extra chinese string is removed and everything else is the same
+        os.chdir(self.cwd)
+        self.assertTrue(filecmp.cmp('./res/values-zh-rTW/strings.xml', './test_cleaned/res/values-zh-rTW/strings.xml'))
+
+
+    def test_getTagByKeyName(self):
+        tags = localizr.getTagsFromTree(self.tree)
+
+        # verify that non-existent key returns None
+        result = localizr.getTagByKeyName(tags, 'doesnotexist')
+        self.assertIsNone(result)
+
+        # verify that returns correct key
+        result = localizr.getTagByKeyName(tags, 'test_1')
+        self.assertEqual(result.attrib['name'], 'test_1')
+
+
+    '''
+    ' look for missing keys in the translated values.xml files
+    '''
+    def test_findMissingKeys(self):
+        keys = localizr.getKeysFromTree(self.tree)
+        missing = localizr.findMissingKeys(keys, self.LANGS, self.res_path)
+
+        # verify that German is not missing any tags
+        self.assertEqual(missing['de'], [])
+
+        # verify that Spanish is missing all tags
+        self.assertEqual(missing['es'], ['test_1', 'test_2', 'test_3', 'test_4', 'test_5'])
+
+        # verify that French is missing all tags (even though it has an extra tag)
+        self.assertEqual(missing['fr'], ['test_1', 'test_2', 'test_3', 'test_4', 'test_5'])
+
+        # verify that traditional Chinese is only missing one tag
+        self.assertEqual(missing['zh-rTW'], ['test_5'])
+
+    '''
+    ' only get language directory in the values-** and values-**-*** format
+    '''
+    def test_getLangsFromDir(self):
+        langs = localizr.getLangsFromDir(self.res_path)
+        self.assertEqual(set(langs), set(self.LANGS))
+
+    '''
+    ' keys should not contain providers or strings marked as not translatable
+    '''
+    def test_getKeyFromTree(self):
+        keys = localizr.getKeysFromTree(self.tree)
+        self.assertEqual(keys, ['test_1', 'test_2', 'test_3', 'test_4', 'test_5'])
+
+    def test_getTagsFromTree(self):
+        tags = localizr.getTagsFromTree(self.tree)
+        # should return all seven tags
+        self.assertEqual(len(tags), 7)
+
+
+        
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ translation_helper.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+
+'''
+This script does two things:
+1) Ouputs strings that haven't been translated in files for each language
+2) Cleans up string.xml files for other languages by removing old strings and 
+re-ordering the strings based on their order in the default language
+
+Other important notes
+-Should support all language codes that follow the -** or -**-*** pattern
+-This ignores strings that start with "provider." or have the "translatable"
+attribute set to "false"
+-The output directory for the missing strings is in the current directory
+-The output file names look like "strings_to_trans-**" where "**" is the language
+code
+'''
+
+import sys
+import os
+import xml.etree.ElementTree as ET
+import xml.dom.minidom as minidom
+import codecs
+import argparse
+
+ORIG_DIR = os.getcwd()
+
+def main(res_path, clean=False):
+    
+    # go to the resource directory and save the whole path
+    os.chdir(res_path)
+    res_path = os.getcwd()
+
+    # get default keys
+    tree = getDefaultTree(res_path)
+    keys = getKeysFromTree(tree)
+    tags = getTagsFromTree(tree)
+
+    # get the languages that we want to translate to    
+    langs = getLangsFromDir(res_path)
+
+    # look for missing keys in each language string file
+    missing = findMissingKeys(keys, langs, res_path)
+
+    # remove old strings and sort them all in the same way
+    if (clean):
+        cleanTranslationFiles(langs, keys, res_path)
+
+    # write files for missing keys for each language
+    createOutputDir()
+    writeMissingKeysToFiles(langs, tags, missing)
+
+def getDefaultTree(res_path):
+    os.chdir(res_path)
+    os.chdir('values')
+    ET.register_namespace('tools', "http://schemas.android.com/tools")
+    return ET.parse('strings.xml')
+
+def createOutputDir():
+    # create output directory
+    os.chdir(ORIG_DIR)
+    if not os.path.exists('to_translate'):
+        os.makedirs('to_translate')        
+
+def writeMissingKeysToFiles(langs, tags, missing):
+    # write xml files for missing strings for each language
+    os.chdir(ORIG_DIR)
+    os.chdir('to_translate')
+    for lang in langs:
+        # skip language if it's not missing any strings
+        if (len(missing[lang]) == 0):
+            continue        
+
+        # create element tree for all the missing tags
+        root = ET.Element("resources")
+        for key in missing[lang]: 
+            tag = getTagByKeyName(tags, key)
+            root.append(tag)
+
+        # write out the 
+        f = codecs.open('strings_to_trans-%s.xml' % (lang), 'wb', 'utf-8')
+        f.write(prettify(root))
+
+
+def getLanguageTrees(langs, res_path):
+    trees = {}
+    for lang in langs:
+        os.chdir(res_path)
+        os.chdir('values-' + lang)
+        trees[lang] = ET.parse('strings.xml')
+    return trees
+
+def cleanTranslationFiles(langs, keys, res_path):
+    trees = getLanguageTrees(langs, res_path)
+    for lang in trees.keys():
+        tree = trees[lang]
+        keys_trans = getKeysFromTree(tree)
+        tags_trans = getTagsFromTree(tree)
+        keys_has = []
+        for key in keys:
+            if key in keys_trans:
+                keys_has.append(key)
+        root = ET.Element("resources")
+        for key in keys_has: 
+            tag = getTagByKeyName(tags_trans, key)
+            root.append(tag)
+
+        # write out file
+        os.chdir(res_path)
+        os.chdir('values-%s' % (lang))
+        f = codecs.open('strings.xml', 'wb', 'utf-8')
+        f.write(prettify(root))
+
+def getTagByKeyName(tags, key):
+    for tag in tags:
+        if tag.attrib['name'] == key:
+            return tag
+
+'''
+Return a pretty-printed XML string for the Element.
+'''
+def prettify(elem):
+    rough_string = ET.tostring(elem, encoding='UTF-8')
+    reparsed = minidom.parseString(rough_string)
+    return u'\n'.join([line for line in reparsed.toprettyxml(indent='\t').split('\n') if line.strip()])
+
+
+def findMissingKeys(keys, langs, res_path):
+    missing = {}
+    trees = getLanguageTrees(langs, res_path)
+    for lang in trees.keys():
+        tree = trees[lang]
+        keys_trans = getKeysFromTree(tree)
+        keys_miss = []
+        for key in keys:
+            if key not in keys_trans:
+                keys_miss.append(key)
+        missing[lang] = keys_miss
+    return missing
+
+def getLangsFromDir(res_path):
+    os.chdir(res_path)
+    langs = []
+    for x in os.walk('.'):
+        if x[0].startswith('./values-'):
+            code = [x[0][9:]][0]
+            if (len(code) == 2) or (len(code) == 6 and code[2] == '-'):
+                langs += [code]
+    return langs
+
+def getKeysFromTree(tree):
+    root = tree.getroot()
+    keys = []
+    for child in root:
+        # ignore strings that can't be translated
+        if ('translatable' in child.attrib.keys() and child.attrib['translatable'] == 'false'):
+            continue
+        # ignore providers
+        if (child.attrib['name'].startswith('provider.')):
+            continue
+        keys.append(child.attrib['name'])
+    return keys
+
+def getTagsFromTree(tree):
+    root = tree.getroot()
+    keys = []
+    for child in root:
+        keys.append(child)
+    return keys
+
+
+if __name__ == '__main__':
+    # parse arguments and do error checking
+    parser = argparse.ArgumentParser()
+    parser.add_argument("res_dir", help="path to the /res directory")
+    parser.add_argument("--clean", help="re-orders and removes strings in the translation files to match the default string ordering",
+                    action="store_true")
+    args = parser.parse_args()
+
+    # run main
+    main(args.res_dir, clean=args.clean)
+
