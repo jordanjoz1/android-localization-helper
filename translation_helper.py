deb18b9e6adbc481a199e769a43facfe9d374bb5
Jordan Jozwiak <jordanjoz1@gmail.com>
2015-01-04 18:22:08 -0800

    fix style to adhere with pep8

--- translation_helper.py
+++ translation_helper.py
@@ -3,7 +3,7 @@
 '''
 This script does two things:
 1) Ouputs strings that haven't been translated in files for each language
-2) Cleans up string.xml files for other languages by removing old strings and 
+2) Cleans up string.xml files for other languages by removing old strings and
 re-ordering the strings based on their order in the default language
 
 Other important notes
@@ -11,8 +11,8 @@ Other important notes
 -This ignores strings that start with "provider." or have the "translatable"
 attribute set to "false"
 -The output directory for the missing strings is in the current directory
--The output file names look like "strings_to_trans-**" where "**" is the language
-code
+-The output file names look like "strings_to_trans-**" where "**" is the
+language code
 '''
 
 import sys
@@ -23,11 +23,12 @@ import argparse
 
 ORIG_DIR = os.getcwd()
 
+
 def main():
 
     # parse command line arguments
     res_path, clean, out_path = parseArgs()
-    
+
     # go to the resource directory and save the whole path
     os.chdir(res_path)
     res_path = os.getcwd()
@@ -37,7 +38,7 @@ def main():
     keys = getKeysFromTree(tree)
     tags = getTagsFromTree(tree)
 
-    # get the languages that we want to translate to    
+    # get the languages that we want to translate to
     langs = getLangsFromDir(res_path)
 
     # look for missing keys in each language string file
@@ -51,20 +52,28 @@ def main():
     createOutputDir(out_path)
     writeMissingKeysToFiles(langs, tags, missing, out_path)
 
+
 def parseArgs():
     # parse arguments and do error checking
     parser = argparse.ArgumentParser()
     parser.add_argument('--res',
-                        help='Path to the app\'s /res directory. If not specifies it assumes current directory',
+                        help='Path to the app\'s /res directory. If not '
+                        'specifies it assumes current directory',
                         default='.')
     parser.add_argument('--output',
-                        help='Path to the output directory. If not specifies it will create a folder called to_translate in the current directory',
+                        help='Path to the output directory. If not specifies '
+                        'it will create a folder called to_translate in the '
+                        'current directory',
                         default='./to_translate')
-    parser.add_argument("--clean", help="re-orders and removes strings in the translation files to match the default string ordering",
-                    action="store_true")
+    parser.add_argument('--clean',
+                        help='re-orders and removes strings in the '
+                        'translation files to match the default string '
+                        'ordering',
+                        action="store_true")
     args = parser.parse_args()
     return args.res, args.clean, args.output
-    
+
+
 def getDefaultTree(res_path):
     os.chdir(res_path)
     os.chdir('values')
@@ -72,11 +81,13 @@ def getDefaultTree(res_path):
     ET.register_namespace('xliff', "urn:oasis:names:tc:xliff:document:1.2")
     return ET.parse('strings.xml')
 
+
 def createOutputDir(out_path):
     # create output directory
     os.chdir(ORIG_DIR)
     if not os.path.exists(out_path):
-        os.makedirs(out_path)        
+        os.makedirs(out_path)
+
 
 def writeMissingKeysToFiles(langs, tags, missing, out_path):
     # write xml files for missing strings for each language
@@ -85,11 +96,11 @@ def writeMissingKeysToFiles(langs, tags, missing, out_path):
     for lang in langs:
         # skip language if it's not missing any strings
         if (len(missing[lang]) == 0):
-            continue        
+            continue
 
         # create element tree for all the missing tags
         root = ET.Element('resources')
-        for key in missing[lang]: 
+        for key in missing[lang]:
             tag = getTagByKeyName(tags, key)
             root.append(tag)
 
@@ -106,6 +117,7 @@ def getLanguageTrees(langs, res_path):
         trees[lang] = ET.parse('strings.xml')
     return trees
 
+
 def cleanTranslationFiles(langs, keys, res_path):
     trees = getLanguageTrees(langs, res_path)
     for lang in trees.keys():
@@ -117,7 +129,7 @@ def cleanTranslationFiles(langs, keys, res_path):
             if key in keys_trans:
                 keys_has.append(key)
         root = ET.Element('resources')
-        for key in keys_has: 
+        for key in keys_has:
             tag = getTagByKeyName(tags_trans, key)
             root.append(tag)
 
@@ -127,20 +139,22 @@ def cleanTranslationFiles(langs, keys, res_path):
         f = codecs.open('strings.xml', 'wb', 'utf-8')
         f.write(prettify(root))
 
+
 def getTagByKeyName(tags, key):
     for tag in tags:
         if tag.get('name') == key:
             return tag
 
-'''
-Return a "pretty-printed" XML string for the Element.
 
-The element tree tostring() preserves the formatting of each individual tag,
-but it can have some funky behavior since we aren't including all the tags
-we read from the original tree.  On Python 3 tostring() does not add the XML
-declaration, so we need to add that manually.
-'''
 def prettify(elem):
+    """Format xml element as a string
+    Return a "pretty-printed" XML string for the Element.
+
+    The element tree tostring() preserves the formatting of each individual
+    tag, but it can have some funky behavior since we aren't including all the
+    tags we read from the original tree.  On Python 3 tostring() does not add
+    the XML declaration, so we need to add that manually.
+    """
     output = ET.tostring(elem, encoding='UTF-8').decode('utf-8')
 
     # make sure we add the xml declaration... stupid python 3
@@ -165,31 +179,33 @@ def findMissingKeys(keys, langs, res_path):
         missing[lang] = keys_miss
     return missing
 
-'''
-Supported langauge directories follow one of two patterns:
-1) values-**
-2) values-**-***
 
-returns code for language or None if not a language directory
-'''
 def getLangDir(dir_name):
+    """
+    Supported langauge directories follow one of two patterns:
+    1) values-**
+    2) values-**-***
+
+    returns code for language or None if not a language directory
+    """
     if dir_name[2:].startswith('values-'):
         code = [dir_name[9:]][0]
         if (len(code) == 2) or (len(code) == 6 and code[2] == '-'):
             return code
-            
+
     # not a language dir
     return None
-    
-    
+
+
 def getLangsFromDir(res_path):
     os.chdir(res_path)
     langs = []
     for x in os.walk('.'):
         code = getLangDir(x[0])
         if code is not None:
-            langs.append(code)		
+            langs.append(code)
     return langs
+
 
 def getKeysFromTree(tree):
     root = tree.getroot()
@@ -204,6 +220,7 @@ def getKeysFromTree(tree):
         keys.append(child.get('name'))
     return keys
 
+
 def getTagsFromTree(tree):
     root = tree.getroot()
     keys = []
@@ -214,4 +231,3 @@ def getTagsFromTree(tree):
 
 if __name__ == '__main__':
     main()
-
